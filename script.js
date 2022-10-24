0c342e65660679f3051e9870ce0b5d8556014313
zabil003 <zabil003@umn.edu>
2022-10-24 00:50:08 -0700

    working on script

---
+++ script.js
@@ -0,0 +1,4 @@
+// Make the top left text "Maxim Zabilo" interactable by the user
+// This means when the user presses on one of the letters just in the string "xim Zabilo"
+// it will switch to red if blue and to blue if red
+// just that part of the string because the "Ma" will always be red 
